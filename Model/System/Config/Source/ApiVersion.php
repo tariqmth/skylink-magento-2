55778c9c3d475e652f32cbe6f1abd22b260b09a5
Ben Corlett <bencorlett@me.com>
2016-06-07 14:24:10 +1000

    Add configuration files to specify connection to Retail Express database.

---
+++ Model/System/Config/Source/ApiVersion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RetailExpress\SkyLinkMagento2\Model\System\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class ApiVersion implements ArrayInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 2, 'label' => __('2')],
+        ];
+    }
+}

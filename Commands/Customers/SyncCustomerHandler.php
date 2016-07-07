7eb26fac03a677c1236d7b22073d53dcd212256c
Ben Corlett <bencorlett@me.com>
2016-07-07 11:20:18 +1000

    Docblocking code.

--- Commands/Customers/SyncCustomerHandler.php
+++ Commands/Customers/SyncCustomerHandler.php
@@ -12,26 +12,61 @@ use RetailExpress\SkyLink\Customers\CustomerId as SkyLinkCustomerId;
 
 class SyncCustomerHandler
 {
-    private $skylinkCustomerRepository;
+    /**
+     * Retail Express Customer Repository.
+     *
+     * @var SkyLinkCustomerRepository
+     */
+    private $skyLinkCustomerRepository;
 
+    /**
+     * Customer Repository.
+     *
+     * @var CustomerRepositoryInterface
+     */
     private $customerRepository;
 
+    /**
+     * Search Criteria Builder, used for building filters against the Customer Repository.
+     *
+     * @var SearchCriteriaBuilder
+     */
     private $searchCriteriaBuilder;
 
+    /**
+     * Customer Service, used for updating/registering Customers.
+     *
+     * @var CustomerService
+     */
     private $customerService;
 
+    /**
+     * Create a new Sync Customer Handler.
+     *
+     * @param SkylinkCustomerRepository   $skyLinkCustomerRepository
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param SearchCriteriaBuilder       $searchCriteriaBuilder
+     * @param CustomerService             $customerService
+     */
     public function __construct(
-        SkylinkCustomerRepository $skylinkCustomerRepository,
+        SkylinkCustomerRepository $skyLinkCustomerRepository,
         CustomerRepositoryInterface $customerRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         CustomerService $customerService
     ) {
-        $this->skylinkCustomerRepository = $skylinkCustomerRepository;
+        $this->skyLinkCustomerRepository = $skyLinkCustomerRepository;
         $this->customerRepository = $customerRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->customerService = $customerService;
     }
 
+    /**
+     * Synchronises a customer by firstly grabbing the customer from Retail Express and then attempts
+     * to match it to an existing Customer in Magento. Depending on whether it finds a match or
+     * not, it'll update an existing Customer in Magento or register a whole new one.
+     *
+     * @param SyncCustomerCommand $command
+     */
     public function handle(SyncCustomerCommand $command)
     {
         $retailExpressCustomer = $this->retrieveRetailExpressCustomer($command->retailExpressCustomerId);
@@ -45,13 +80,29 @@ class SyncCustomerHandler
         }
     }
 
+    /**
+     * Retrieves a Retail Express Customer from the given numerical representation of it's ID.
+     *
+     * @param int $retailExpressCustomerId
+     *
+     * @return SkyLinkCustomer
+     */
     private function retrieveRetailExpressCustomer($retailExpressCustomerId)
     {
         $retailExpressCustomerId = new SkyLinkCustomerId($retailExpressCustomerId);
 
-        return $this->skylinkCustomerRepository->find($retailExpressCustomerId);
+        return $this->skyLinkCustomerRepository->find($retailExpressCustomerId);
     }
 
+    /**
+     * Finds an existing Customer within Magento that matches the given Retail Express Customer.
+     *
+     * @param SkyLinkCustomer $retailExpressCustomer
+     *
+     * @return CustomerInterface
+     *
+     * @throws TooManyCustomerMatchesException
+     */
     private function findExistingCustomer(SkyLinkCustomer $retailExpressCustomer)
     {
         $retailExpressCustomerId = $retailExpressCustomer->getId();
@@ -73,11 +124,26 @@ class SyncCustomerHandler
         }
     }
 
+    /**
+     * Instructs the Customer Service to update a given Customer with the information from a SkyLinkCustomer.
+     *
+     * Used mainly to force type-hints in tests.
+     *
+     * @param Customer        $customer
+     * @param SkyLinkCustomer $retailExpressCustomer
+     */
     private function updateCustomer(CustomerInterface $customer, SkyLinkCustomer $retailExpressCustomer)
     {
         $this->customerService->updateCustomer($customer, $retailExpressCustomer);
     }
 
+    /**
+     * Instructs the Customer Service to regsiter a new Customer with the information from a SkyLinkCustomer.
+     *
+     * Used mainly to force type-hints in tests.
+     *
+     * @param SkyLinkCustomer $retailExpressCustomer
+     */
     private function registerCustomer(SkyLinkCustomer $retailExpressCustomer)
     {
         $this->customerService->registerCustomer($retailExpressCustomer);
--- Commands/Customers/TooManyCustomerMatchesException.php
+++ Commands/Customers/TooManyCustomerMatchesException.php
@@ -7,6 +7,14 @@ use RetailExpress\SkyLink\Customers\CustomerId as SkyLinkCustomerId;
 
 class TooManyCustomerMatchesException extends LocalizedException
 {
+    /**
+     * Create an new Exception with the given Retail Express Customer ID and a numerical number of matches.
+     *
+     * @param SkyLinkCustomerId $retailExpressCustomerId
+     * @param int               $matches
+     *
+     * @return TooManyCustomerMatchesException
+     */
     public static function withRetailExpressCustomerId(SkyLinkCustomerId $retailExpressCustomerId, $matches)
     {
         return new self(__(

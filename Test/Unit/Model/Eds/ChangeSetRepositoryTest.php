83a0cf930c6ae5c9c360e7e3c70f5b9a4f3deebc
Ben Corlett <bencorlett@me.com>
2016-07-12 09:41:32 +1000

    Add empty test class for Change Set Repositories.

---
+++ Test/Unit/Model/Eds/ChangeSetRepositoryTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RetailExpress\SkyLink\Magento2\Test\Unit\Model\Eds;
+
+use PHPUnit_Framework_TestCase;
+
+class ChangeSetRepositoryTest extends PHPUnit_Framework_TestCase
+{
+    //
+}

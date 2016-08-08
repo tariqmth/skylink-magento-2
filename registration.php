b881f845fc2239c06c33affdfc03cb2391b1d4d1
Ben Corlett <bencorlett@me.com>
2016-08-08 12:09:53 +1000

    Adjust files that are included in phpspec code coverage reports.

--- registration.php
+++ registration.php
@@ -2,10 +2,8 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-// @codeCoverageIgnoreStart
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'RetailExpress_SkyLinkMagento2',
     __DIR__
 );
-// @codeCoverageIgnoreEnd

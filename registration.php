96eae305482517ce59bd026385336abce040269c
Ben Corlett <bencorlett@me.com>
2016-08-08 10:50:09 +1000

    Ignore registration file in tests.

--- registration.php
+++ registration.php
@@ -2,8 +2,10 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
+// @codeCoverageIgnoreStart
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'RetailExpress_SkyLinkMagento2',
     __DIR__
 );
+// @codeCoverageIgnoreEnd

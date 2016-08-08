6500f0e5a31956fd76d5efdab08776c8bd571fd6
Ben Corlett <bencorlett@me.com>
2016-08-08 12:09:16 +1000

    Switch numbers for words in spec function names.

--- spec/Model/System/Config/Source/SalesChannelIdSpec.php
+++ spec/Model/System/Config/Source/SalesChannelIdSpec.php
@@ -3,22 +3,21 @@
 namespace spec\RetailExpress\SkyLink\Magento2\Model\System\Config\Source;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use RetailExpress\SkyLink\Magento2\Model\System\Config\Source\SalesChannelId;
 
 class SalesChannelIdSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(SalesChannelId::class);
     }
 
-    function it_should_have_50_options()
+    public function it_should_have_fifty_options()
     {
         $this->toOptionArray()->shouldHaveCount(50);
     }
 
-    function its_first_option_should_be_1_not_0()
+    public function its_first_option_should_be_one_not_zero()
     {
         $this->toOptionArray()[0]['value']->shouldBe(1);
     }

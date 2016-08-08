4f3481c9bad097e32425c0c77329abad4f598aad
Ben Corlett <bencorlett@me.com>
2016-08-08 12:10:56 +1000

    Remove old interfaces as new ones replace them.

---
+++ Api/Customers/SkyLinkCustomerServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RetailExpress\SkyLink\Magento2\Api\Customers;
+
+use Magento\Customer\Api\Data\CustomerInterface;
+use RetailExpress\SkyLink\Customers\Customer as SkyLinkCustomer;
+
+interface SkyLinkCustomerServiceInterface
+{
+    /**
+     * Register a new SkyLink Customer from the given SkyLink Customer.
+     *
+     * @param CustomerInterface $magentoCustomer
+     */
+    public function registerSkyLinkCustomer(CustomerInterface $magentoCustomer);
+
+    /**
+     * Updates the given SkyLink Customer with the information from the Magento Customer.
+     *
+     * @param SkyLinkCustomer   $skyLinkCustomer
+     * @param CustomerInterface $magentoCustomer
+     */
+    public function updateSkyLinkCustomer(SkyLinkCustomer $skyLinkCustomer, CustomerInterface $magentoCustomer);
+}

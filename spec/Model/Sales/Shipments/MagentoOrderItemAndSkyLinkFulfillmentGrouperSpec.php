d075e8f822ed5b7090a2301d732e3747ea7d7314
Ben Corlett <bencorlett@me.com>
2016-12-12 14:56:16 +1100

    Green up tests.

--- spec/Model/Sales/Shipments/MagentoOrderItemAndSkyLinkFulfillmentGrouperSpec.php
+++ spec/Model/Sales/Shipments/MagentoOrderItemAndSkyLinkFulfillmentGrouperSpec.php
@@ -3,10 +3,20 @@
 namespace spec\RetailExpress\SkyLink\Model\Sales\Shipments;
 
 use PhpSpec\ObjectBehavior;
+use RetailExpress\SkyLink\Api\Catalogue\Products\MagentoSimpleProductRepositoryInterface;
 use RetailExpress\SkyLink\Model\Sales\Shipments\MagentoOrderItemAndSkyLinkFulfillmentGrouper;
 
 class MagentoOrderItemAndSkyLinkFulfillmentGrouperSpec extends ObjectBehavior
 {
+    private $magentoSimpleProductRepository;
+
+    public function let(MagentoSimpleProductRepositoryInterface $magentoSimpleProductRepository)
+    {
+        $this->magentoSimpleProductRepository = $magentoSimpleProductRepository;
+
+        $this->beConstructedWith($this->magentoSimpleProductRepository);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(MagentoOrderItemAndSkyLinkFulfillmentGrouper::class);

1f51f6e9329b945984b5ef1ecd82b0aeb052d732
Ben Corlett <bencorlett@me.com>
2016-12-12 14:57:38 +1100

    Start tests on Exceptions.

---
+++ spec/Exceptions/Sales/Orders/MagentoOrderItemNotLinkedToSkyLinkProductExceptionSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec\RetailExpress\SkyLink\Exceptions\Sales\Orders;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use RetailExpress\SkyLink\Exceptions\Sales\Orders\MagentoOrderItemNotLinkedToSkyLinkProductException;
+
+class MagentoOrderItemNotLinkedToSkyLinkProductExceptionSpec extends ObjectBehavior
+{
+    private $magentoProductId;
+
+    public function let()
+    {
+        $this->magentoProductId = 1;
+    }
+
+    public function it_is_initializable()
+    {
+        $this->beConstructedThrough('withMagentoProductId', [$this->magentoProductId]);
+        $this->shouldHaveType(MagentoOrderItemNotLinkedToSkyLinkProductException::class);
+    }
+
+    public function it_has_a_useful_error_message()
+    {
+        $this->beConstructedThrough('withMagentoProductId', [$this->magentoProductId]);
+        $this->getMessage()->shouldBe('Magento Product #1 is not linked to a SkyLink Product, cannot use in order.');
+    }
+}

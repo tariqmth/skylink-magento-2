b53c23e5b77dc2d9239df93701be4418a06ae236
Ben Corlett <bencorlett@me.com>
2016-12-21 12:19:04 +1100

    Fix typo.

--- Commands/Customers/SyncSkyLinkCustomerToMagentoCustomerHandler.php
+++ Commands/Customers/SyncSkyLinkCustomerToMagentoCustomerHandler.php
@@ -43,7 +43,7 @@ class SyncSkyLinkCustomerToMagentoCustomerHandler
     /**
      * Logger instance.
      *
-     * @var LoggerInterface
+     * @var SkyLinkLoggerInterface
      */
     private $logger;
 

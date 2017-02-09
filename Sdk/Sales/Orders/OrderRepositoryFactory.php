86d8a2207759b507292dc77f6f2dfef6843f0c74
Ben Corlett <bencorlett@me.com>
2017-02-09 12:13:59 +1100

    Remove all traces of old V2 Order Shim.

--- Sdk/Sales/Orders/OrderRepositoryFactory.php
+++ Sdk/Sales/Orders/OrderRepositoryFactory.php
@@ -2,9 +2,7 @@
 
 namespace RetailExpress\SkyLink\Sdk\Sales\Orders;
 
-use Magento\Framework\App\Filesystem\DirectoryList;
 use RetailExpress\SkyLink\Api\ConfigInterface;
-use RetailExpress\SkyLink\Api\Sales\Orders\ConfigInterface as OrderConfigInterface;
 use RetailExpress\SkyLink\Sdk\Apis\V2Factory as V2ApiFactory;
 use RetailExpress\SkyLink\Model\Factory;
 use RetailExpress\SkyLink\Sdk\V2OrderShim\DefaultBulkOrderCacher;
@@ -20,20 +18,10 @@ class OrderRepositoryFactory
 
     private $v2ApiFactory;
 
-    private $directoryList;
-
-    private $orderConfig;
-
-    public function __construct(
-        ConfigInterface $config,
-        V2ApiFactory $v2ApiFactory,
-        DirectoryList $directoryList,
-        OrderConfigInterface $orderConfig
-    ) {
+    public function __construct(ConfigInterface $config, V2ApiFactory $v2ApiFactory)
+    {
         $this->config = $config;
         $this->v2ApiFactory = $v2ApiFactory;
-        $this->directoryList = $directoryList;
-        $this->orderConfig = $orderConfig;
     }
 
     public function create()

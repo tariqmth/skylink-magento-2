e3780aabcce935ce33b832f4d2e66e79965f167b
Ben Corlett <bencorlett@me.com>
2017-03-08 17:16:01 +1100

    Work on improving the interaction with checking ETA by adding feedback when it fails.

--- view/frontend/web/eta/button.js
+++ view/frontend/web/eta/button.js
@@ -3,6 +3,7 @@ define(['jquery'], function ($) {
     var $button = $(config.button);
     var $responseWrapper = $(config.response_wrapper);
     var $responseDate = $(config.response_date);
+    var $failedResponseWrapper = $(config.failed_response_wrapper);
 
     // Watches for the "Check ETA" button to be tapped and fetches the ETA
     $button.on('mouseup', function (e) {
@@ -15,13 +16,22 @@ define(['jquery'], function ($) {
 
     // Checks the ETA date and updates the DOM
     function checkEta() {
-      $.get(config.eta_url, function (response) {
-        var date = new Date(response.date);
+      $.getJSON(config.eta_url, function (response) {
+        if (typeof repsonse.date !== 'undefined') {
+          var date = new Date(response.date);
 
-        $responseDate.html(date.toLocaleDateString());
+          $responseDate.html(date.toLocaleDateString());
+        } else {
+          $responseDate.html(config.no_eta_text);
+        }
+
         $button.addClass('checked');
         $responseWrapper.removeClass('not-ready');
       })
+        .fail(function () {
+          $button.addClass('checked');
+          $failedResponseWrapper.removeClass('not-ready');
+        });
     }
 
   };

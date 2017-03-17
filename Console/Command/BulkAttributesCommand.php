e90933868f363185c8d1754fd0c94b4a797c4b59
Ben Corlett <bencorlett@me.com>
2017-03-17 16:43:17 +1100

    Add console command for bulk attributes syncing.

---
+++ Console/Command/BulkAttributesCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace RetailExpress\SkyLink\Console\Command;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use RetailExpress\CommandBus\Api\CommandBusInterface;
+use RetailExpress\SkyLink\Api\Catalogue\Attributes\SkyLinkAttributeCodeRepositoryInterface;
+use RetailExpress\SkyLink\Api\ConfigInterface;
+use RetailExpress\SkyLink\Commands\Catalogue\Attributes\SyncSkyLinkAttributeToMagentoAttributeCommand;
+use RetailExpress\SkyLink\Sdk\Catalogue\Attributes\AttributeCode as SkyLinkAttributeCode;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BulkAttributesCommand extends Command
+{
+    private $skyLinkAttributeCodeRepository;
+
+    private $commandBus;
+
+    public function __construct(
+        SkyLinkAttributeCodeRepositoryInterface $skyLinkAttributeCodeRepository,
+        CommandBusInterface $commandBus
+    ) {
+        $this->skyLinkAttributeCodeRepository = $skyLinkAttributeCodeRepository;
+        $this->commandBus = $commandBus;
+
+        parent::__construct('retail-express:skylink:bulk-attributes');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setDescription('Gets a list of attributes from Retail Express and queues a job for each one to sync to a Magento attribute');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $progressBar = new ProgressBar($output);
+        $progressBar->start();
+
+        /* @var SkyLinkAttributeCode[] $skyLinkAttributeCodes */
+        $skyLinkAttributeCodes = $this->skyLinkAttributeCodeRepository->getList();
+
+        // Loop over our Price Groups and add dispatch a command to sync each
+        array_walk($skyLinkAttributeCodes, function (SkyLinkAttributeCode $skyLinkAttributeCode) use ($progressBar) {
+
+            $command = new SyncSkyLinkAttributeToMagentoAttributeCommand();
+            $command->skyLinkAttributeCode = (string) $skyLinkAttributeCode;
+
+            $this->commandBus->handle($command);
+
+            $progressBar->advance();
+        });
+
+        $progressBar->finish();
+        $output->writeln('');
+        $output->writeln(sprintf(<<<'MESSAGE'
+<info>%s Retail Express attributes have had jobs queued to sync them to Magento attributes.
+Ensure that an instance of 'retail-express:command-bus:consume-queue attributes' is running to perform the actual sync.</info>
+MESSAGE
+            ,
+            count($skyLinkAttributeCodes)
+        ));
+    }
+}

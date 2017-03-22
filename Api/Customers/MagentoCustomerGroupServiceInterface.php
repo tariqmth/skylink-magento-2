483e018afcee89535c9e68e24c1ccbdbd208aebf
Ben Corlett <bencorlett@me.com>
2017-03-22 11:38:19 +1100

    Add logging to customer group syncing and also update existing customer groups.

--- Api/Customers/MagentoCustomerGroupServiceInterface.php
+++ Api/Customers/MagentoCustomerGroupServiceInterface.php
@@ -2,6 +2,7 @@
 
 namespace RetailExpress\SkyLink\Api\Customers;
 
+use Magento\Customer\Api\Data\GroupInterface;
 use RetailExpress\SkyLink\Sdk\Customers\PriceGroups\PriceGroup as SkyLinkPriceGroup;
 
 interface MagentoCustomerGroupServiceInterface
@@ -14,4 +15,12 @@ interface MagentoCustomerGroupServiceInterface
      * @return \Magento\Customer\Api\Data\GroupInterface
      */
     public function createMagentoCustomerGroup(SkyLinkPriceGroup $skyLinkPriceGroup);
+
+    /**
+     * Updates the Magento Customer Group from the given SkyLink Price Group.
+     *
+     * @param GroupInterface    $magentoCustomerGroup
+     * @param SkyLinkPriceGroup $skyLinkPriceGroup
+     */
+    public function updateMagentoCustomerGroup(GroupInterface $magentoCustomerGroup, SkyLinkPriceGroup $skyLinkPriceGroup);
 }
--- Commands/Customers/SyncSkyLinkPriceGroupToMagentoCustomerGroupHandler.php
+++ Commands/Customers/SyncSkyLinkPriceGroupToMagentoCustomerGroupHandler.php
@@ -2,10 +2,12 @@
 
 namespace RetailExpress\SkyLink\Commands\Customers;
 
+use Magento\Framework\Event\ManagerInterface as EventManagerInterface;
 use RetailExpress\SkyLink\Api\Customers\MagentoCustomerGroupRepositoryInterface;
 use RetailExpress\SkyLink\Api\Customers\MagentoCustomerGroupServiceInterface;
 use RetailExpress\SkyLink\Sdk\Customers\PriceGroups\PriceGroupKey as SkyLinkPriceGroupKey;
 use RetailExpress\SkyLink\Sdk\Customers\PriceGroups\PriceGroupRepositoryFactory;
+use RetailExpress\SkyLink\Api\Debugging\SkyLinkLoggerInterface;
 
 class SyncSkyLinkPriceGroupToMagentoCustomerGroupHandler
 {
@@ -15,14 +17,32 @@ class SyncSkyLinkPriceGroupToMagentoCustomerGroupHandler
 
     private $magentoCustomerGroupService;
 
+    /**
+     * Logger instance.
+     *
+     * @var SkyLinkLoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Event Manager instance.
+     *
+     * @var EventManagerInterface
+     */
+    private $eventManager;
+
     public function __construct(
         PriceGroupRepositoryFactory $skyLinkPriceGroupRepositoryFactory,
         MagentoCustomerGroupRepositoryInterface $magentoCustomerGroupRepository,
-        MagentoCustomerGroupServiceInterface $magentoCustomerGroupService
+        MagentoCustomerGroupServiceInterface $magentoCustomerGroupService,
+        SkyLinkLoggerInterface $logger,
+        EventManagerInterface $eventManager
     ) {
         $this->skyLinkPriceGroupRepositoryFactory = $skyLinkPriceGroupRepositoryFactory;
         $this->magentoCustomerGroupRepository = $magentoCustomerGroupRepository;
         $this->magentoCustomerGroupService = $magentoCustomerGroupService;
+        $this->logger = $logger;
+        $this->eventManager = $eventManager;
     }
 
     public function handle(SyncSkyLinkPriceGroupToMagentoCustomerGroupCommand $command)
@@ -35,13 +55,37 @@ class SyncSkyLinkPriceGroupToMagentoCustomerGroupHandler
         /* @var \RetailExpress\SkyLink\Sdk\Customers\PriceGroups\PriceGroup $skyLinkPriceGroup */
         $skyLinkPriceGroup = $skyLinkPriceGroupRepository->find($skyLinkPriceGroupKey);
 
+        $this->logger->info('Syncing SkyLink Price Group to Magento Customer Group', [
+            'SkyLink Price Group ID' => $skyLinkPriceGroup->getKey()->getId(),
+            'SkyLink Price Group Name' => $skyLinkPriceGroup->getNameWithType(),
+        ]);
+
         /* @var \Magento\Customer\Api\Data\GroupInterface|null $magentoCustomerGroup */
         $magentoCustomerGroup = $this->magentoCustomerGroupRepository->findBySkyLinkPriceGroupKey($skyLinkPriceGroupKey);
 
         if (null === $magentoCustomerGroup) {
+            $this->logger->debug('No Magento Customer Group exists for SkyLink Price Group, creating one.', [
+                'SkyLink Price Group ID' => $skyLinkPriceGroup->getKey()->getId(),
+                'SkyLink Price Group Name' => $skyLinkPriceGroup->getNameWithType(),
+            ]);
+
             $magentoCustomerGroup = $this->magentoCustomerGroupService->createMagentoCustomerGroup($skyLinkPriceGroup);
+        } else {
+            $this->logger->debug('Found existing Magento Customer Group for SkyLink Price Group, updating.', [
+                'SkyLink Price Group ID' => $skyLinkPriceGroup->getKey()->getId(),
+                'SkyLink Price Group Name' => $skyLinkPriceGroup->getNameWithType(),
+                'Magento Customer Group ID' => $magentoCustomerGroup->getId(),
+                'Magento Customer Group Code' => $magentoCustomerGroup->getCode(),
+            ]);
         }
 
-        // @todo add logging and event triggers
+        $this->eventManager->dispatch(
+            'retail_express_skylink_skylink_price_group_was_synced_to_magento_customer_group',
+            [
+                'command' => $command,
+                'skylink_price_group' => $skyLinkPriceGroup,
+                'magento_customer_group' => $magentoCustomerGroup,
+            ]
+        );
     }
 }

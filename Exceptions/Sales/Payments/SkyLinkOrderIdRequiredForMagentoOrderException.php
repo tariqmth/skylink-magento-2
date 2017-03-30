5aa199711faa5bbbcdd4a1baa20b9952db72c092
Ben Corlett <bencorlett@me.com>
2017-03-30 19:02:45 +1100

    Fix placeholders in error message.

--- Exceptions/Sales/Payments/SkyLinkOrderIdRequiredForMagentoOrderException.php
+++ Exceptions/Sales/Payments/SkyLinkOrderIdRequiredForMagentoOrderException.php
@@ -10,7 +10,7 @@ class SkyLinkOrderIdRequiredForMagentoOrderException extends LocalizedException
     public static function withMagentoOrder(OrderInterface $magentoOrder)
     {
         return new self(__(
-            'Magento Order #%s does not have a SkyLink Order ID associated with it, cannot add payment from invoice.',
+            'Magento Order #%1 does not have a SkyLink Order ID associated with it, cannot add payment from invoice.',
             $magentoOrder->getIncrementId()
         ));
     }

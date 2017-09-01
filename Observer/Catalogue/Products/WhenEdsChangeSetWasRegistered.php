2737ed252e11292247521270dbe61307dd4b82a9
Ben Corlett <bencorlett@me.com>
2017-09-01 12:58:20 +1000

    When there are multiple entities in a regular EDS call, treat them as a bulk sync. [Testing:54]

--- Observer/Catalogue/Products/WhenEdsChangeSetWasRegistered.php
+++ Observer/Catalogue/Products/WhenEdsChangeSetWasRegistered.php
@@ -29,8 +29,14 @@ class WhenEdsChangeSetWasRegistered implements ObserverInterface
             }
         }, $changeSet->getEntities()));
 
+        $isBulk = count($commands) > 1;
+
         // Loop through and execute our commands
-        array_map(function ($command) use ($changeSet) {
+        array_map(function ($command) use ($changeSet, $isBulk) {
+            if (true === $isBulk) {
+                $command->potentialCompositeProductRerun = true;
+            }
+
             $command->changeSetId = $changeSet->getId()->toNative();
             $this->commandBus->handle($command);
         }, $commands);
